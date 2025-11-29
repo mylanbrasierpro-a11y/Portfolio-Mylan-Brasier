363b679db67a94d3f8f254b5700aabe256845a12
Mylan Brasier <mylanbrasierpro@gmail.com>
2025-11-29 12:55:30 +0100

    start hover header and accueil

---
+++ script.js
@@ -0,0 +1,8 @@
+let index = 0;
+const images = document.querySelectorAll(".carousel img");
+
+setInterval(() => {
+  images[index].classList.remove("active");
+  index = (index + 1) % images.length;
+  images[index].classList.add("active");
+}, 1500);
